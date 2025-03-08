abaef4495fb4f3ec54f217b9579dcbda159e925d
Ver1ta5 <95023787+Ver1ta5@users.noreply.github.com>
2025-03-09 03:11:29 +0800

    Add files via upload

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,42 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   _555_png;
+    const int            _555_pngSize = 24904;
+
+    extern const char*   _556_png;
+    const int            _556_pngSize = 29129;
+
+    extern const char*   vecteezy_circletechnologybackgroundfuturisticconceptwithcircuit_6430152_jpg;
+    const int            vecteezy_circletechnologybackgroundfuturisticconceptwithcircuit_6430152_jpgSize = 254471;
+
+    extern const char*   _557_png;
+    const int            _557_pngSize = 10239;
+
+    extern const char*   _558_png;
+    const int            _558_pngSize = 59892;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 5;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
